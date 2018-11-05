5fc8c3c5fb681496ec9027e2aaa4b1d1b25b826a
Vitale Petrovich <43952517+VitalePetrovich@users.noreply.github.com>
2018-11-05 20:09:18 +0300

    Add files via upload

--- WeatherStation/Program.cs
+++ WeatherStation/Program.cs
@@ -1,9 +1,21 @@
-﻿namespace WeatherStation
+﻿using ObserverPatternDemo.Implemantation.Observable;
+using ObserverPatternDemo.Implemantation.Observers;
+
+namespace WeatherStation
 {
     class Program
     {
         static void Main(string[] args)
         {
+            WeatherData waetherStation = new WeatherData();
+            CurrentConditionsReport conditionsReport = new CurrentConditionsReport();
+            StatisticReport statisticReport = new StatisticReport();
+
+            waetherStation.Register(conditionsReport);
+            waetherStation.Register(statisticReport);
+            waetherStation.Unregister(statisticReport);
+           
+            waetherStation.StartNotify();
         }
     }
 }
